9828e79b49d7f87ebfe5b5bd026c058d12220021
Jeffrey Wong <jeffrey.wong.businessmail@gmail.com>
2024-03-26 00:02:13 -0400

    Draft 2
    figuring out how to extract the input and save the info in a list

---
+++ content.js
@@ -0,0 +1,14 @@
+const generateHTML = (pageName) => {
+    return 
+}
+
+switch(window.location.hostname) {
+    case "www.youtube.com":
+        alert("YOU ARE DISTRACTED")
+        break;
+    case "www.instagram.com":
+        alert("YOU ARE DISTRACTED")
+            break;
+}
+
+const blackListElement = document.getElementById("blackList")
